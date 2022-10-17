9d80fe57e08cac6c001867d22f94f3f05d66d779
ChristopherWilliamBill <christ.william.bill@gmail.com>
2022-10-17 21:01:00 +0700

    coba

--- lib/pg.ts
+++ lib/pg.ts
@@ -1,13 +1,32 @@
+//LOCAL
+// import { Pool } from "pg";
+
+// let conn: any;
+
+// if (!conn) {
+//   conn = new Pool({
+//     user: "christopher",
+//     host: "localhost",
+//     port: 5432,
+//     database: "christopher",
+//   });
+// }
+
+// export { conn };
+
+
+//HEROKU
 import { Pool } from "pg";
 
 let conn: any;
 
 if (!conn) {
   conn = new Pool({
-    user: "christopher",
-    host: "localhost",
-    port: 5432,
-    database: "christopher",
+    user: process.env.USER,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DATABASE,
+    ssl: true
   });
 }
 
