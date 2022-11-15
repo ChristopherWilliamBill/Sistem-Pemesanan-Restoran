3f1426de232d6e235eeaf06985e3663690e5204a
ChristopherWilliamBill <christ.william.bill@gmail.com>
2022-11-15 11:41:56 +0700

    menampilkan antrian pesanan

--- pages/admin/antrianpesanan.js
+++ pages/admin/antrianpesanan.js
@@ -1,13 +1,28 @@
 import Layout from '../../component/layout'
 import {conn} from '../../lib/pg.ts';
-
+import styles from '../../styles/AntrianPesanan.module.css'
 
 export default function AntrianPesanan({dataOrder, dataMenu}){
     return(
         <>
-          {console.log(dataOrder)}
           {console.log(dataMenu)}
 
+          <div className={styles.container}>
+
+            {
+              dataOrder.map( //untuk setiap data pesanan
+                d => //membuat div yang isinya data pesanan tersebut
+                  <div className={styles.ordercard}> 
+                    <p>Meja: {d.idMeja}</p>
+                    <p>Waktu dipesan: {d.time.split(".")[0]}</p>
+
+                    {d.idMenu.split(",").map(Number).map( //idMenu yang tadinya string dipecah menjadi array setiap kali ditemukan "," dan dijadikan int menggunakan .map(Number)
+                      (order,index) => <p>{dataMenu[order - 1].namaMenu} x {d.count.split(",")[index]}</p> //setiap idMenu yang dipesan, tampilkan namanya dari dataMenu, dan jumlahnya
+                    )}
+                  </div>
+              )
+            }
+          </div>
         </>
     )
 }
@@ -20,6 +35,7 @@ export async function getServerSideProps(){
   const queryMenu = `SELECT * FROM "Menu"`
   const resMenu = await conn.query(queryMenu)
   const dataMenu = resMenu.rows
+  dataMenu.sort((a,b) => a.id - b.id)
   return{
     props:{
       dataMenu,
