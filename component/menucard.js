d6f938061cae11ce5dae79a5c2b23aa9f5f85b83
ChristopherWilliamBill <christ.william.bill@gmail.com>
2023-03-15 23:31:07 +0700

    foto + css

--- component/menucard.js
+++ component/menucard.js
@@ -1,4 +1,5 @@
 import styles from '../styles/MenuCard.module.css'
+import Image from 'next/image'
 
 export default function MenuCard({menu, addToOrder, addToInputOrderTambahan, extendOrder}){
 
@@ -12,9 +13,18 @@ export default function MenuCard({menu, addToOrder, addToInputOrderTambahan, ext
 
     return(
         <div className={styles.menucard} onClick={() => handleOrder(menu)}>
+            <div className={styles.imagecontainer}>
+                <Image 
+                    src="/v1678895940/3ayamgoreng_l0ynmq.jpg"
+                    width={600}
+                    height={400}
+                    
+                    //onError={handleError}
+                />
+            </div>
             <h3>{menu.namaMenu}</h3>
             <p>{menu.deskripsiMenu}</p>
-            <p>IDR {menu.harga.toLocaleString()}</p>
+            <p><b>IDR {menu.harga.toLocaleString()}</b></p>
         </div>
     )
 }
