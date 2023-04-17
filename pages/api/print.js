6b6190ad07b3414ab13867f18c2ef72ac8466a0f
ChristopherWilliamBill <christ.william.bill@gmail.com>
2023-04-17 12:44:50 +0700

    style to print

--- pages/api/print.js
+++ pages/api/print.js
@@ -25,31 +25,7 @@ export default async (req, res) => {
   
     const options = { encoding: "GB18030" /* default */ }
     const printer = new escpos.Printer(device, options);
-    console.log(device)
-
-    device.open(function(error){
-        printer
-        .font('a')
-        .align('ct')
-        .style('bu')
-        .size(1, 1)
-        .text('The quick brown fox jumps over the lazy dog')
-        .text('敏捷的棕色狐狸跳过懒狗')
-        .barcode('1234567', 'EAN8')
-        .table(["One", "Two", "Three"])
-        .tableCustom(
-          [
-            { text:"Left", align:"LEFT", width:0.33, style: 'B' },
-            { text:"Center", align:"CENTER", width:0.33},
-            { text:"Right", align:"RIGHT", width:0.33 }
-          ],
-          { encoding: 'cp857', size: [1, 1] } // Optional
-        )
-        .qrimage('https://github.com/song940/node-escpos', function(err){
-          this.cut();
-          this.close();
-        });
-      });    printer
+    printer
     .font('a')
     .align('ct')
     .style('bu')
@@ -82,7 +58,7 @@ export default async (req, res) => {
     //         }
     //     );
     // }
-
+2
     console.log(request)
 
     try{
