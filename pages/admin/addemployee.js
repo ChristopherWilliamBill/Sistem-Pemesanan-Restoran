8a279abcf3f6a5385190ed65e35b987749f9ded1
ChristopherWilliamBill <christ.william.bill@gmail.com>
2023-04-17 10:34:20 +0700

    style

---
+++ pages/admin/addemployee.js
@@ -0,0 +1,35 @@
+import Layout from '../../component/layout'
+import styles from '../../styles/AddEmployee.module.css'
+import { useSession } from 'next-auth/react'
+import FormKaryawan from '../../component/formkaryawan'
+import NavBar from '../../component/navbar'
+
+export default function AddEmployee(){
+
+  const { data: session, status } = useSession()
+
+  if (status === "authenticated") {
+    return(
+      <>
+        {session.role === "manager" ? 
+          <div className={styles.container}>
+            <h1>Add Employee</h1>
+            <FormKaryawan></FormKaryawan>
+          </div>
+          : 'Only for manager'
+        }
+      </>
+    )
+  }
+}
+
+AddEmployee.getLayout = function getLayout(page) {
+    return (
+      <Layout>
+        <div className={styles.rootcontainer}>
+          <NavBar key='tambahkaryawan' currentPath={'/admin/addemployee'}></NavBar>
+          {page}
+        </div>
+      </Layout>
+    )
+}
---
+++ pages/admin/addmenu.js
@@ -0,0 +1,59 @@
+import Layout from '../../component/layout'
+import { useRouter } from 'next/router'
+import styles from '../../styles/AddMenu.module.css'
+import { useSession } from 'next-auth/react'
+import FormMenu from '../../component/formmenu'
+import {conn} from '../../module/pg.js';
+import NavBar from '../../component/navbar'
+
+export default function AddMenu({dataMenu}){
+  const router = useRouter()
+
+  const { data: session, status } = useSession()
+
+  if (status === "authenticated") {
+    return(
+      <div className={styles.container}>
+        <FormMenu selectedMenu={null} dataMenu={dataMenu} idAdmin={session.idAdmin}></FormMenu>
+      </div>
+    )
+  }
+}
+
+export async function getStaticProps(){
+  const query = `SELECT * FROM "Menu"`
+  const queryPaket = `SELECT "Menu"."idMenu", "TerdiriMenu"."isiMenu", "TerdiriMenu"."jumlah" FROM "Menu" INNER JOIN "TerdiriMenu" ON "Menu"."idMenu" = "TerdiriMenu"."idMenu"`
+
+  const res = await conn.query(query)
+  const resPaket = await conn.query(queryPaket)
+
+  const dataMenu = res.rows
+  const dataPaket = resPaket.rows
+
+  dataMenu.sort((a,b) => a.idMenu - b.idMenu)
+
+  for(let i = 0; i < dataMenu.length; i++){
+    dataMenu[i].isiMenu = []
+  }
+
+  for(let i = 0; i < dataPaket.length; i++){
+    dataMenu[dataPaket[i].idMenu - 1].isiMenu.push({isiMenu: dataPaket[i].isiMenu, jumlah: dataPaket[i].jumlah})
+  }
+  
+  return{
+    props:{
+      dataMenu
+    }
+  }
+}
+
+AddMenu.getLayout = function getLayout(page) {
+    return (
+      <Layout>
+        <div className={styles.rootcontainer}>
+          <NavBar key='tambahmenu' currentPath={'/admin/addmenu'}></NavBar>
+          {page}
+        </div>
+      </Layout>
+    )
+}
